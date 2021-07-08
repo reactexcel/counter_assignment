e7af36b4c67c951bcd88b2969bc0980e39769906
reactexcel <arun.excel2011@gmail.com>
2021-07-08 12:43:10 +0530

    added stop and start timer

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -5,15 +5,23 @@ import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 const Counter = ({ value, handleRemoveCounter }) => {
   const [timer, settimer] = useState(0);
+  const [timerStatus, setTimerStatus] = useState(false);
   const intervalId = useRef();
 
   const increaseCount = () => settimer((prevCount) => prevCount + 1);
 
   useEffect(() => {
-    intervalId.current = setInterval(increaseCount, 1000);
+    if (timerStatus) {
+      intervalId.current = setInterval(increaseCount, 1000);
+    } else if (!timerStatus && timer !== 0) {
+      clearInterval(intervalId.current);
+    }
     return () => clearInterval(intervalId.current);
-  }, [timer]);
+  }, [timer, timerStatus]);
 
+  const handleActive = () => {
+    setTimerStatus(!timerStatus);
+  };
   return (
     <div
       style={{
@@ -26,6 +34,12 @@ const Counter = ({ value, handleRemoveCounter }) => {
       <div style={{ alignSelf: "center" }}>
         <Chip label={timer} variant='outlined' />
       </div>
+      <button
+        className='btn btn-lg btn-block p-2 btn-light'
+        onClick={handleActive}
+      >
+        {timerStatus ? "Stop " : "Start"}
+      </button>
       <IconButton onClick={() => handleRemoveCounter(value.id)}>
         <DeleteForeverIcon />
       </IconButton>
